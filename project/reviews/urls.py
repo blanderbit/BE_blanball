8f14a6f9520c43e9846a11cdb14cd8648c17c9fa
Djoncarton1921 <shapranov.nik@gmail.com>
2022-08-31 11:53:08 +0300

    review upddate

--- project/reviews/urls.py
+++ project/reviews/urls.py
@@ -7,7 +7,10 @@ urlpatterns = [
    # endpoint where the user can create review
     path('client/review/create', ReviewCreate.as_view(),
       name="review-create"),
-     # endpoint where admin can check her reviews list
+     # endpoint where client can check her reviews list
     path('client/my/reviews/list', UserReviewsList.as_view(),
       name="my-reviews"),
+    # endpoint where client can update her reviews
+    path('client/review/<int:pk>', PutDeleteReview.as_view(),
+      name="update-review"),
 ]
