d15169c6453804ee411ddc699eb4a96bbe623919
Djoncarton1921 <shapranov.nik@gmail.com>
2022-10-06 11:06:27 +0300

    reset project to fix auto deploy in portainer

--- compose/local/django-start.sh
+++ compose/local/django-start.sh
@@ -4,5 +4,5 @@ python manage.py makemigrations
 python manage.py migrate
 # python manage.py test
 # gunicorn project.wsgi:application --bind 0.0.0.0:8000
-# python3 manage.py runserver 0.0.0.0:8000
-uwsgi --ini uwsgi.ini
+python3 manage.py runserver 0.0.0.0:8000
+# uwsgi --ini uwsgi.ini
