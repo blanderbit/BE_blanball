61d114ae574c19b5c012985bc635a30832a721ec
Djoncarton1921 <shapranov.nik@gmail.com>
2022-10-08 15:57:22 +0300

    bugfix urls.py

--- project/project/urls.py
+++ project/project/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import serve
 
 from django.urls import path,include
 from django.conf.urls.static import static
-from django.
+from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
