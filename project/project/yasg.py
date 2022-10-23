42ea42b07a1f5ac11c1161a3812056238d3a0a3c
Djoncarton1921 <shapranov.nik@gmail.com>
2022-10-23 16:37:54 +0300

    update path to swagger

--- project/project/yasg.py
+++ project/project/yasg.py
@@ -1,36 +1,29 @@
 import json
 
-from typing import Union
-
-from django.urls import path
-from django.urls.resolvers import (
-   URLResolver, 
-   URLPattern,
-)
+from django.urls import re_path
 
 from rest_framework import permissions
 
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+
 def get_current_version_for_swagger():
    with open('./project/config.json', 'r') as f:
       json_data = json.load(f)
       return json_data['version']
 
-
 schema_view = get_schema_view(
    openapi.Info(
-      title = 'Blanball',
+      title='Blanball',
       default_version = get_current_version_for_swagger(),
-      license = openapi.License(name='BSD License'),
+      license=openapi.License(name='BSD License'),
    ),
-   public = True,
+   public=True,
    permission_classes=(permissions.AllowAny, ),
 )
 
-urlpatterns: list[Union[URLResolver, URLPattern]] = [
-   path('/api/v1/swagger/', schema_view.with_ui('swagger', cache_timeout = 0), 
-      name = 'schema-swagger-ui'),
+urlpatterns = [
+   re_path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), 
+      name='schema-swagger-ui'),
 ]
-
