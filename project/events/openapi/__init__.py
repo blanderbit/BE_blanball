d02a35e72515936fea248c34b5b1f409cc19d589
Djoncarton1921 <shapranov.nik@gmail.com>
2023-01-14 12:22:52 +0200

    BLAN-732 - refactored swagger openpi documentation for filters in the  events block

---
+++ project/events/openapi/__init__.py
@@ -0,0 +1,43 @@
+#==============================================================
+# Filter and ordering query params
+
+from events.openapi.openapi_filters import (
+    events_type_query as events_type_query
+)
+from events.openapi.openapi_filters import (
+    events_gender_query as events_gender_query
+)
+from events.openapi.openapi_filters import (
+    events_status_query as events_status_query
+)
+from events.openapi.openapi_filters import (
+    events_duration_query as events_duration_query
+)
+from events.openapi.openapi_filters import (
+    events_need_ball_query as events_need_ball_query
+)
+from events.openapi.openapi_filters import (
+    events_relevant_searh_query as events_relevant_searh_query
+)
+from events.openapi.openapi_filters import (
+    events_searh_query as events_searh_query
+)
+from events.openapi.openapi_filters import (
+    events_ordering_query as events_ordering_query
+)
+from events.openapi.openapi_filters import (
+    event_date_and_time_before_query as event_date_and_time_before_query
+)
+#==============================================================
+
+#==============================================================
+# list-documented query parameters for use in view classes
+
+from events.openapi.openapi_filters import (
+    events_list_query_params as events_list_query_params
+)
+from events.openapi.openapi_filters import (
+    events_relevant_list_query_params as events_relevant_list_query_params
+)
+
+#==============================================================
