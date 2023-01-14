ffecd3138786794bb348f7d96efb71ddfec06691
Djoncarton1921 <shapranov.nik@gmail.com>
2023-01-14 12:16:57 +0200

    BLAN-732 -  refactored swagger openpi documentation for filters in the authentication block

---
+++ project/authentication/openapi/__init__.py
@@ -0,0 +1,39 @@
+#==============================================================
+# Filter and ordering query params
+
+from authentication.openapi.openapi_filters import (
+    users_searh_query as users_searh_query
+)
+from authentication.openapi.openapi_filters import (
+    users_relevant_searh_query as users_relevant_searh_query
+)
+from authentication.openapi.openapi_filters import (
+    users_profile__position_query as users_profile__position_query
+)
+from authentication.openapi.openapi_filters import (
+    users_profile__gender_query as users_profile__gender_query
+)
+from authentication.openapi.openapi_filters import (
+    users_is_online_query as users_is_online_query
+)
+from authentication.openapi.openapi_filters import (
+    users_ordering as users_ordering
+)
+from authentication.openapi.openapi_filters import (
+    users_profile_age_min_query as users_profile_age_min_query
+)
+from authentication.openapi.openapi_filters import (
+    users_profile_age_max_query as users_profile_age_max_query
+)
+#==============================================================
+
+#==============================================================
+# list-documented query parameters for use in view classes
+
+from authentication.openapi.openapi_filters import (
+    users_list_query_params as users_list_query_params
+)
+from authentication.openapi.openapi_filters import (
+    users_relevant_list_query_params as users_relevant_list_query_params
+)
+#==============================================================
