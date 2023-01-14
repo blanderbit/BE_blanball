1cca020f395e4c380f2de79089c778e74561d002
Djoncarton1921 <shapranov.nik@gmail.com>
2023-01-14 12:52:20 +0200

    BLAN-732 - refactored swagger openpi documentation for filters in the api_keys block - fixed bug with getting not exist user reviews list

--- project/reviews/services.py
+++ project/reviews/services.py
@@ -6,21 +6,24 @@ from rest_framework.exceptions import (
     PermissionDenied,
 )
 from reviews.models import Review
+from config.exceptions import (
+    _404
+)
 
 
 def hide_user_reviews(func: Callable[[], QuerySet[Review]]):
-    def wrap(self) -> QuerySet[Review]:
+    def wrap(self, *args, **kwargs) -> QuerySet[Review]:
         try:
             user: User = User.objects.get(id=self.kwargs["pk"])
             if user.configuration["show_reviews"]:
                 return func(self)
             elif user == self.request.user:
-                return func(self)
+                return func(self, *args, **kwargs)
             else:
                 raise PermissionDenied()
         except User.DoesNotExist:
-            pass
+            raise _404(object=User)
         except KeyError:
-            return func(self)
+            return func(self, *args, **kwargs)
 
     return wrap
